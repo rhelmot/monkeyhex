2d264d9357a86337da8cfb2c571c2fc36b60f6d3
Andrew Dutcher <andrew@andrewdutcher.com>
2017-09-21 16:50:32 -0700

    Tick version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ Monkeyhex, as the name suggests, monkeypatches the system displayhook as well as
 
 from distutils.core import setup
 setup(name='monkeyhex',
-      version='1.3',
+      version='1.4',
       py_modules=['monkeyhex'],
       description='Monkeypatch the python interpreter and debugger to print integer results in hex',
       long_description=long_description,
