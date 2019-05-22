e586517e6b880e72ee883c0dcbb6bb9030240748
Audrey Dutcher <audrey@rhelmot.io>
2019-05-22 02:43:01 -0700

    switch to setuptools; tick again for good measure

--- setup.py
+++ setup.py
@@ -4,9 +4,9 @@ monkeyhex.py is a small library to assist users of the python shell who work in 
 Monkeyhex, as the name suggests, monkeypatches the system displayhook as well as the pprint and pdb modules to format integers as hex. To use it, just import the library and all future results will be formatted. To view a result in decimal again, put the expression in a print statement.
 '''
 
-from distutils.core import setup
+from setuptools import setup
 setup(name='monkeyhex',
-      version='1.7',
+      version='1.7.1',
       py_modules=['monkeyhex'],
       description='Monkeypatch the python interpreter and debugger to print integer results in hex',
       long_description=long_description,
